4c4adec7b0593a6ffc5fb79bd63735cbf649f2ff
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-09 23:00:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,28 @@
 import { useEffect, useState } from "react";
 
-type Respo = {
-  id: number;
-  name: string;
-  favorite?: boolean;
+type Location = {
+  latitude: any;
+  longitude: any;
 };
 
 function App() {
-  const [repositories, setRepositories] = useState<Respo[]>([]);
+  const [location, setLocation] = useState<Location>({});
 
   useEffect(() => {
-    fetch("https://api.github.com/users/MatheusFellipi/repos").then((res) => {
-      res.json().then((res) => {
-        setRepositories(res);
-      });
-    });
+    const watchId = navigator.geolocation.watchPosition(handlePositionReceived);
+    return () => navigator.geolocation.clearWatch(watchId);
   }, []);
 
-  useEffect(() => {
-    const filtetred = repositories.filter((repo) => repo.favorite);
-    document.title = `Vote tem ${filtetred.length} favoritos`;
-  }, [repositories]);
-
-  function handleFAvorite(id: number) {
-    const newRepos = repositories.map((repo) => {
-      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
-    });
-
-    setRepositories(newRepos);
+  function handlePositionReceived({ coords }: any) {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude, longitude });
   }
 
   return (
     <div>
-      <ul>
-        {repositories.map((repo) => (
-          <li key={repo.id}>
-            {repo.name}
-            {repo.favorite && <span> favorito</span>}
-            <button onClick={() => handleFAvorite(repo.id)}>Favoritar</button>
-          </li>
-        ))}
-      </ul>
+      {" "}
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude} <br />
     </div>
   );
 }
